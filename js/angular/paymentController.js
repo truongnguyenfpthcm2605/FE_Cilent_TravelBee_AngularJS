480b6c2d4abc803d572f7aa4640bd9a34cf35d8b
= <=>
2023-11-30 18:51:35 +0700

    fix

--- js/angular/paymentController.js
+++ js/angular/paymentController.js
@@ -1,9 +1,31 @@
-app.controller("paymentController", function($scope, $http, $rootScope,$location) {
+app.controller("paymentController", function ($scope, $http, $rootScope, $location) {
 
-   $scope.orders ={}
+     $scope.orders = {}
+     $scope.orders.voucher = 'N'
+     let labelvoucher = document.getElementById("label-voucher")
 
-   $scope.payment = function(){
-        console.log($scope.orders)
-   }
 
- });
+     $scope.payment = function () {
+          console.log($scope.orders)
+          $location.url("m/ain")
+     }
+
+     
+
+     $scope.checkvoucher = function () {
+          console.log($scope.orders.voucher)
+          $http.get($rootScope.url + '/api/v1/home/voucher/' + $scope.orders.voucher)
+               .then(function (response) {
+                    labelvoucher.innerHTML = response.data.title + ' Giảm giá ' + response.data.discount +"%"
+                    labelvoucher.style.color = "green"
+                    labelvoucher.style.fontSize = "15px"
+               })
+               .catch(function (error) {
+                    labelvoucher.innerHTML = 'Không tìm thấy voucher'
+                    labelvoucher.style.color = "red"
+                    labelvoucher.style.fontSize = "15px"
+               });
+
+     }
+
+});
